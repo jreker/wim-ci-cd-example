1efcd7ba0143a67f16dc641572167d1c8218c266
Johanness Reker <j.Reker@ktr.com>
2022-07-18 13:49:00 +0200

    add import for get method

--- src/test/java/de/jreker/wim/calculator/RestControllerIntegrationTest.java
+++ src/test/java/de/jreker/wim/calculator/RestControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package de.jreker.wim.calculator;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
