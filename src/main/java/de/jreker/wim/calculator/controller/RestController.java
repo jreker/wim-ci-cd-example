919096b0138db1d4fbae888cd06eeaf4897d6865
Johanness Reker <j.Reker@ktr.com>
2022-07-19 15:27:12 +0200

    feature-e

--- src/main/java/de/jreker/wim/calculator/controller/RestController.java
+++ src/main/java/de/jreker/wim/calculator/controller/RestController.java
@@ -34,7 +34,7 @@ public class RestController {
 
     @GetMapping("/version")
     public ResponseEntity<?> getVersion() {
-        return ResponseEntity.ok("Version 2.2 - Feature-D-Branch");
+        return ResponseEntity.ok("Version 2.2 - Feature-E-Branch");
     }
 
     @GetMapping("/potd")
